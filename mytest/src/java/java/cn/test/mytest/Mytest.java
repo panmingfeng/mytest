0dfe32d69cd582102057aa113a21aa46af4c4da1
pzq <710219127@qq.com>
2018-12-25 11:09:21 +0800

    java

---
+++ mytest/src/java/java/cn/test/mytest/Mytest.java
@@ -0,0 +1,9 @@
+package cn.test.mytest;
+
+public class Mytest {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("1111111111111111111");
+        System.out.println("2222222222222222222");
+    }
+}
