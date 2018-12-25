d89f175959fce1b905df5f02cc1f0808780fa950
pzq <710219127@qq.com>
2018-12-25 10:59:55 +0800

    test提交2222

--- mytest/src/test/java/cn/test/mytest/Mytest.java
+++ mytest/src/test/java/cn/test/mytest/Mytest.java
@@ -4,5 +4,6 @@ public class Mytest {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("1111111111111111111");
+        System.out.println("2222222222222222222");
     }
 }
