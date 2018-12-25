6f19e89a9e14623cc763311d959952d779968b0f
pzq <710219127@qq.com>
2018-12-25 10:53:29 +0800

    java目录修改

--- mytest/src/test/java/cn/test/mytest/Mytest.java
+++ mytest/src/test/java/cn/test/mytest/Mytest.java
@@ -3,5 +3,6 @@ package cn.test.mytest;
 public class Mytest {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("1111111111111111111");
     }
 }
